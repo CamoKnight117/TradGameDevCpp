b091470c3dcde5bce384ee9fce107a54844458c8
benrose19 <benreader1148@gmail.com>
2023-03-07 18:41:07 -0700

    Got things running, added minion selected highlight

--- UnrealTestCPPProject/TestProject/Intermediate/Build/Win64/TestProjectEditor/DebugGame/Engine/SharedPCH.Engine.NonOptimized.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30146
-#include "C:/Users/u1354966/Documents/GitHub/TradGameDevCpp/UnrealTestCPPProject/TestProject/Intermediate/Build/Win64/TestProjectEditor/DebugGame/Engine/SharedPCH.Engine.NonOptimized.ShadowErrors.h"
--- UnrealTestCPPProject/TestProject/Intermediate/Build/Win64/TestProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ UnrealTestCPPProject/TestProject/Intermediate/Build/Win64/TestProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.29.30148
-#include "D:/squis/Documents/SchoolStuff/Spring2023/GameDev/Repo/TradGameDevCpp/UnrealTestCPPProject/TestProject/Intermediate/Build/Win64/TestProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.29.30147
+#include "C:/Users/benre/OneDrive/Documents/GitHub/TradGameDevCpp/UnrealTestCPPProject/TestProject/Intermediate/Build/Win64/TestProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
